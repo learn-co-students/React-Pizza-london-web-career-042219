17c9b9daa87453f68fb6312e5f0e14d9e748ef83
Lauren Carne <oreocookie@hotmail.co.uk>
2019-07-01 19:17:37 +0100

    not quite finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import PizzaForm from "./components/PizzaForm";
+import PizzaList from "./containers/PizzaList";
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pizzas: [],
+      selectedPizza: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPizzasFromServer();
+  }
+
+  fetchPizzasFromServer = () => {
+    fetch("http://localhost:3000/pizzas")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          pizzas: data
+        })
+      );
+  };
+
+  handleClick = pizza => {
+    this.setState({
+      selectedPizza: pizza
+    });
+  };
+
+  submitPizza = e => {
+    console.log(e);
+  };
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm
+          pizza={this.state.selectedPizza}
+          submitPizza={this.submitPizza}
+        />
+        <PizzaList pizzas={this.state.pizzas} handleClick={this.handleClick} />
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { Component } from "react";
 
-const PizzaForm = () => {
-  return(
+class PizzaForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false
+    };
+  }
+  clicked = () => {
+    this.setState({
+      checked: !this.state.checked
+    });
+  };
+  render() {
+    return (
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Pizza Topping"
+            defaultValue={this.props.pizza.topping}
+          />
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select defaultValue={this.props.pizza.size} className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +32,36 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Vegetarian
-            </label>
+            <input
+              onClick={this.clicked}
+              className="form-check-input"
+              type="radio"
+              value="Vegetarian"
+            />
+            <label className="form-check-label">Vegetarian</label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
-            <label className="form-check-label">
-              Not Vegetarian
-            </label>
+            <input
+              onClick={this.clicked}
+              className="form-check-input"
+              type="radio"
+              value="Not Vegetarian"
+            />
+            <label className="form-check-label">Not Vegetarian</label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button
+            type="submit"
+            className="btn btn-success"
+            onClick={this.props.submitPizza()}
+          >
+            Submit
+          </button>
         </div>
       </div>
-
-  )
+    );
+  }
 }
 
-export default PizzaForm
+export default PizzaForm;
